f03be2fe191af02395a5541579ce34cacbbd0908
Administrator <likipp@163.com>
2021-03-05 14:28:14 +0800

    定义公用消息结构

---
+++ utils/msg/errors.go
@@ -0,0 +1,5 @@
+package msg
+
+var (
+	DoNothing = "数据未变更"
+)
--- utils/msg/message.go
+++ utils/msg/message.go
@@ -4,6 +4,7 @@ var (
 	CreatedSuccess = "创建成功"
 	CreatedFail    = "创建失败"
 	UpdatedSuccess = "更新成功"
+	UpdatedFail    = "更新失败"
 	DeletedSuccess = "删除成功"
 	DeletedFail    = "删除失败"
 	GetSuccess     = "查询成功"
